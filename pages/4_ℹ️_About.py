d6d4d7b31319b0ca85f1ec8a43999faa48632159
TomMonks <thomas.m.monks@gmail.com>
2025-01-28 21:48:52 +0000

    chore(title): about title change

--- pages/4_ℹ️_About.py
+++ pages/4_ℹ️_About.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
-st.title("Model Tutorial")
-
-
-st.link_button("@TheOpenScienceNerd YouTube", "https://www.youtube.com/@TheOpenScienceNerd")
-
+st.title("Code your first DES model in Python Tutorial")
+st.link_button("@OpenScienceNerd YouTube", "https://www.youtube.com/@TheOpenScienceNerd")
 
 VIDEO_URL = "https://www.youtube.com/watch?v=FZOnGolWgho"
 st.video(VIDEO_URL)
