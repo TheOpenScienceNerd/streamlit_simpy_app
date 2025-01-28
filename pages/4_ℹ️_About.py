9a800bf31cb000b5993c78d9b3c4d7bb35dfcf4a
TomMonks <thomas.m.monks@gmail.com>
2025-01-28 21:43:58 +0000

    feat(about): link to youtube video

---
+++ pages/4_ℹ️_About.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Model Tutorial")
+
+
+st.link_button("@TheOpenScienceNerd YouTube", "https://www.youtube.com/@TheOpenScienceNerd")
+
+
+VIDEO_URL = "https://www.youtube.com/watch?v=FZOnGolWgho"
+st.video(VIDEO_URL)
